7300e96c3a359a16f75bffaf1d4b1a37d14da9b8
valera_kudryavtsev <shrhannor@gmail.com>
2022-02-18 12:50:16 +0300

    task1 update 2x

--- src/task1/MainClass.java
+++ src/task1/MainClass.java
@@ -1,7 +1,29 @@
 package task1;
 
-public class MainClass {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class MainClass implements Predicate {
+    public static void main(String[] args) {
+        Integer num;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input number: ");
+        num = sc.nextInt();
+        Predicate<Integer> predicate = x -> testFunc(x);
+        System.out.println(predicate.test(num));
+    }
+
+    public static boolean testFunc(Integer num) {
+        while (num != 1 && num % 2 == 0) {
+            num /= 2;
+        }
+        if (num==1)
+            return true;
+        else
+            return false;
+    }
+
+    @Override
+    public boolean test(Integer value) {
+        return false;
     }
 }
---
+++ src/task1/Predicate.java
@@ -0,0 +1,5 @@
+package task1;
+
+public interface Predicate<T> {
+    boolean test(Integer value);
+}
